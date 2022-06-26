e756e930d0888fb6cc90478fca77830a58f84591
max <azevedomaximilian@gmail.com>
2022-06-26 11:15:38 +0200

    bug fixes and optimisations

--- src/run.py
+++ src/run.py
@@ -3,7 +3,8 @@ from travelling_salesman import *
 
 
 # travelling salesman problem initialisation
-tsp = TSP(15, 1000)
+num_pnts = 100               # default: 25
+tsp = TSP(num_pnts, 2200)
 tsp.calc_points_dist()
 #tsp.init_rand_table(1000, 0.001)
 #print(f'Adjacency Matrix:\n{tsp.adj_matrix}\n')
@@ -25,7 +26,7 @@ def algorithm_avg(its, alg=0):
 
         else:
             start = time.time()
-            c_cost = tsp.compute_loc_beam_search(4, lb_its=40)
+            c_cost = tsp.compute_loc_beam_search(4, lb_its=100)  # 3, 75
             end = time.time()
             c_cost = c_cost[0]
 
@@ -35,31 +36,75 @@ def algorithm_avg(its, alg=0):
     return avg_cost/its, avg_time/its
 
 
+def tsp_plot(tsp, order=None):
+        pnt_x, pnt_y = [], []
+
+        if order is None:
+            pnt_x = tsp.points[:, 0]
+            pnt_y = tsp.points[:, 1]
+        
+        else:
+            for i in order:
+                pnt_x.append(tsp.points[i][0])
+                pnt_y.append(tsp.points[i][1])
+
+        plt.scatter(pnt_x, pnt_y)
+        plt.scatter(pnt_x[0], pnt_y[0], color='red')    # starting point
+        plt.plot(pnt_x, pnt_y, color='red')
+
+
+        plt.show()
+
 
 
 # algorithm measurements
-'''
-msr_iters = 20
+
+msr_iters = 1
 print(algorithm_avg(msr_iters, alg=0))
 print(algorithm_avg(msr_iters, alg=1))
 print(algorithm_avg(msr_iters, alg=2))
-'''
+
 
 # hill climbing solution
+s_hc = time.time()
 hc_opt, hc_cost = tsp.compute_hill_climb()
+e_hc = time.time()
 print(f'(HC)[Final]\t Route is hc_opt with cost \t{hc_cost}\n')
 
 # simulated annealing solution
+s_sa = time.time()
 sa_opt, sa_cost = tsp.compute_sim_annealing()
+e_sa = time.time()
 print(f'(SA)[Final]\t Route is sa_opt with cost \t{sa_cost}\n')
 
 # local beam search solution
-lb_ret = tsp.compute_loc_beam_search(4, lb_its=100)
-print(f'(LB)[Final]\t Route is sa_opt with cost \t{lb_ret[0]}')
+s_lb = time.time()
+lb_ret = tsp.compute_loc_beam_search(8, lb_its=100)
+e_lb = time.time()
+print(f'(LB)[Final]\t Route is lb_opt with cost \t{lb_ret[0]}')
 
 
-# plot routes
-tsp.tsp_plot()
-tsp.tsp_plot(hc_opt)
-tsp.tsp_plot(sa_opt)
-tsp.tsp_plot(lb_ret[1])
+# plot data
+rand_route, rand_cost = tsp.gen_rand_route()
+rand_cost = round(rand_cost, 2)
+plt.title(f'Random Route for {num_pnts} Points\n')
+plt.figtext(.5, .9, f'path cost: {rand_cost}, time: 0s', ha="center")
+tsp_plot(tsp, rand_route)
+
+hc_time = round(e_hc - s_hc, 4)
+hc_cost = round(hc_cost, 2)
+plt.title(f'Hill Climbing for {num_pnts} Points\n')
+plt.figtext(.5, .9, f'path cost: {hc_cost}, time: {hc_time}s', ha="center")
+tsp_plot(tsp, order=hc_opt)
+
+sa_time = round(e_sa - s_sa, 4)
+sa_cost = round(sa_cost, 2)
+plt.title(f'Simulated Annealing for {num_pnts} Points\n')
+plt.figtext(.5, .9, f'path cost: {sa_cost}, time: {sa_time}s', ha="center")
+tsp_plot(tsp, order=sa_opt)
+
+lb_time = round(e_lb - s_lb, 4)
+lb_cost = round(lb_ret[0], 2)
+plt.title(f'Local Beam Search for {num_pnts} Points\n')
+plt.figtext(.5, .9, f'path cost: {lb_cost}, time: {lb_time}s', ha="center")
+tsp_plot(tsp, lb_ret[1])
--- src/travelling_salesman.py
+++ src/travelling_salesman.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import math
+import heapq
 from matplotlib import pyplot as plt
 
 
@@ -39,71 +40,49 @@ class TSP:
         self.adj_matrix = self.adj_matrix + self.adj_matrix.T - np.diag(self.adj_matrix.diagonal())
 
 
-    def tsp_plot(self, order=None):
-        pnt_x, pnt_y = [], []
-
-        if order is None:
-            pnt_x = self.points[:, 0]
-            pnt_y = self.points[:, 1]
-        
-        else:
-            for i in order:
-                pnt_x.append(self.points[i][0])
-                pnt_y.append(self.points[i][1])
-
-        plt.scatter(pnt_x, pnt_y)
-        plt.plot(pnt_x, pnt_y, color='red')
-        plt.show()
-
-
-    def successor_states(self, route, dist_old):
+    def successor_states(self, route, dist_old):    # O(n^2)
         # simple way of generating successor nodes
         succs = []
-        dist_new = []
-        for i in range(0, len(route)-2):
+        for i in range(0, len(route)-2):        # O( (n^2-n)/2 - (n+1) ) or O(n^2)
             for j in range(i+1, len(route)-1):
                 swap = route[:]     # pass by value
                 s_i, s_j = i+1, j+1
 
-                dist_aft = self.swap_dist_cost(swap, s_i, s_j)
+                dist_aft = self.swap_dist_cost(swap, s_i, s_j)  # O(1)
 
                 # swap nodes
                 swap[s_i], swap[s_j] = route[s_j], route[s_i]
 
                 dist_bef = self.swap_dist_cost(swap, s_i, s_j)
                 new_cost = dist_old - dist_aft + dist_bef
-                d_err = new_cost - dist_old
 
-                test = self.calc_route_cost(swap)
-                #print("A", test, dist_old - dist_aft + dist_bef)
+                succs.append((new_cost, swap))
 
-                succs.append(swap)
-                dist_new.append(new_cost)
-
-        return succs, dist_new
+        heapq.heapify(succs)    # O(n)
+        return succs
 
 
-    def calc_route_cost(self, route):
+    def calc_route_cost(self, route):   # O(n)
         # sum over values of arr[i][i+1] for i in route. (i, i+1) is an edge from route[i] to route[i+1]
         return sum([self.adj_matrix[route[i]][route[i+1]] for i in range(len(route)-1)])
 
 
-    def swap_dist_cost(self, route, i, j):
+    def swap_dist_cost(self, route, i, j):  # O(1)
         p_1 = min(i, j)     # smallest index of swapped elems
         p_2 = max(i, j)     # biggest index of swapped elems
 
-        dist = []
+        dist = 0
 
-        dist.append(self.adj_matrix[route[p_2-1]][route[p_2]])
+        dist += self.adj_matrix[route[p_2-1]][route[p_2]]
         if p_2 < self.size-1:
-            dist.append(self.adj_matrix[route[p_2]][route[p_2+1]])
+            dist += self.adj_matrix[route[p_2]][route[p_2+1]]
 
         if p_2-1 != p_1:    # otherwise we calculate this distance twice
-            dist.append(self.adj_matrix[route[p_1]][route[p_1+1]])
+            dist += self.adj_matrix[route[p_1]][route[p_1+1]]
 
-        dist.append(self.adj_matrix[route[p_1-1]][route[p_1]])    # always greater than 0
+        dist += self.adj_matrix[route[p_1-1]][route[p_1]]    # always greater than 0
 
-        return sum(dist)
+        return dist
 
 
     def gen_rand_route(self):
@@ -120,31 +99,27 @@ class TSP:
             route, route_cost = self.gen_rand_route()
             #print(f'(HC)[Init]\t Route is route with cost \t{route_cost}')
 
-            min_route = route_cost
+            min_route_cost = route_cost
             moved = True
 
             while moved:
                 moved = False
                 # generate successor nodes
-                successors, succ_costs = self.successor_states(route, min_route)
-
+                successors = self.successor_states(route, min_route_cost)   # O(n^2)
                 # find successor node with minimum route cost
-                for i, s in enumerate(successors):
-                    if succ_costs[i] < min_route:
-                        route = s
-                        min_route = succ_costs[i]
-
-                        moved = True
+                s_cost, s = successors[0]
+                if s_cost < min_route_cost: moved = True
+                min_route_cost, route = s_cost, s
 
             # compute best route among multiple hill climb attempts (random-restart hill climbing)
-            if min_route < best_route_cost:
+            if min_route_cost < best_route_cost:
                 best_route = route
-                best_route_cost = min_route
+                best_route_cost = min_route_cost
 
         return best_route, best_route_cost
 
 
-    def compute_sim_annealing(self, init_temp=500, thr=0.07, d_t=0.075):
+    def compute_sim_annealing(self, init_temp=500, thr=0.07, d_t=0.00075):    # default: 500, 0.07, 0.075
         route, route_cost = self.gen_rand_route()
         #print(f'(SA)[Init]\t Route is route with cost \t{route_cost}')
 
@@ -156,7 +131,7 @@ class TSP:
 
             # generate random successor node by swapping some i, j > 1, and calculate its value
             rand_scc = route[:]
-            i, j = random.sample(range(len(route) - 1), 2)
+            i, j = random.sample(range(len(route) - 1), 2)  # choose two random indices two swap
             i, j = i+1, j+1
 
             # used a few lines ahead
@@ -167,9 +142,8 @@ class TSP:
 
             # compute error efficiently by only checking swap distances
             dist_bef = self.swap_dist_cost(rand_scc, i, j)
-            new_cost = route_cost + dist_aft - dist_bef
-            d_err = -(new_cost - route_cost)
-            #d_err_bad = self.calc_route_cost(rand_scc) - route_cost 
+            new_cost = route_cost - dist_aft + dist_bef
+            d_err = new_cost - route_cost
 
             # accept node if better or if worse by some probability
             if d_err < 0 or math.exp(-d_err/temp) > random.random():
@@ -184,38 +158,26 @@ class TSP:
         curr_routes = []
         for i in range(k):
             route, route_cost = self.gen_rand_route()
-            #print(f'(LB)[Init]\t Route {i} is route with cost \t{route_cost}')
-
             curr_routes.append((route_cost, route))
 
-        moved = True
-        cnt = 0
-        while moved and cnt < lb_its:
+        while lb_its > 0:   # O(k*n^2)
+            all_succs = []
 
-            moved = True
-            k_best_routes = []
-            routes_arr = [i[1] for i in curr_routes]
-
-            # find k best successor nodes
-            for i, route in enumerate(routes_arr):
+            # add all successor nodes to a list
+            for (cost, route) in curr_routes:
                 # generate successor nodes
-                successors, succ_costs = self.successor_states(route, curr_routes[i][0])   # ii
+                successors = self.successor_states(route, cost)             # O(n^2)
+                all_succs += [heapq.heappop(successors) for _ in range(k)]  # O(log(m)) = O(k*log(m)) for m heap size
 
-                # find successor node with minimum route cost
-                for i, s in enumerate(successors):
-                    #route_cost = self.calc_route_cost(s)
-                    route_cost = succ_costs[i]
-                    
-                    # check if current node is better than the highest val of the best
-                    k_best_routes.append((route_cost, s))
-                    if len(k_best_routes) > k:
-                        k_best_routes.remove(max(k_best_routes))
-            
-            curr_routes = k_best_routes[:]
-            cnt += 1
+            # find successor node with minimum route cost with heap
+            heapq.heapify(all_succs)    # O(k^2) but k is often small
+            for i in range(k):          # O(k*log(k^2)) = O(k^2)
+                curr_routes.append(heapq.heappop(all_succs))
 
-        min_key = min(curr_routes, key=lambda t : t[0])
-        return min_key
+            curr_routes = curr_routes[-k:]
+            lb_its -= 1
+
+        return curr_routes[0]
 
     
     def compute_gen_algorithm(self):
